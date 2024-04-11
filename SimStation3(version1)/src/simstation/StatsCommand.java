4a805589562504a6d6c2d96965531350e871dfa0
Nicole Nacua <nicole.nacua@sjsu.edu>
2024-04-11 14:48:39 -0700

    Added StatsCommand

---
+++ SimStation3(version1)/src/simstation/StatsCommand.java
@@ -0,0 +1,22 @@
+package simstation;
+import mvc.Command;
+import mvc.Model;
+
+public class StatsCommand extends Command {
+    public StatsCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation sim = (Simulation)model;
+        sim.startTimer();
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        sim.stopTimer();
+    }
+}
