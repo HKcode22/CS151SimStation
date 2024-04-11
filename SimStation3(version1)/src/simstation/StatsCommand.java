8bb790fe58bb65271cc21fe43995a4b03f8cfcd8
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-11 15:16:22 -0700

    Update StatsCommand.java

--- SimStation3(version1)/src/simstation/StatsCommand.java
+++ SimStation3(version1)/src/simstation/StatsCommand.java
@@ -20,3 +20,25 @@ public class StatsCommand extends Command {
         sim.stopTimer();
     }
 }
+
+
+//package simstation;
+//
+//import mvc.Command;
+//import mvc.Model;
+//
+//public class StartCommand extends Command {
+//
+//    public StartCommand(Model model) {
+//        super(model);
+//    }
+//
+//    @Override
+//    public void execute() {
+//        System.out.println("Start button clicked!"); // Debugging print statement
+//        ((Simulation) model).start();
+//    }
+//
+//
+//}
+
