487f59f338ac319a62d2be820f771f0ff25656d9
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-11 15:15:27 -0700

    Update Simulation.java

--- SimStation3(version1)/src/simstation/Simulation.java
+++ SimStation3(version1)/src/simstation/Simulation.java
@@ -1,7 +1,10 @@
 package simstation;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import java.util.*;
 import mvc.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Simulation extends Model {
 
@@ -23,7 +26,75 @@ public class Simulation extends Model {
             clock++;
         }
     }
-
+    
     // etc.
+
+    public List<Agent> agents;
+
+    public boolean run;
+    
+    public width;
+    public height;
+
+    public Simulation(){
+        agents = new ArrayList<>();
+        run = false;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+    public void addAgent(Agent agent){
+        agents.add(agent)
+    }
+
+    public List<Agent> getAgents(){
+        return agents
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void start() {
+        if (!running) { // checking if simulatuon is running 
+            running = true; 
+            System.out.println("Simulation started!!!!"); 
+
+            for (Agent agent : agents) {
+                agent.start();
+            }
+
+
+            Thread updateThread = new Thread(() -> {
+                while (running) { // continue updating agents while simulation is runing
+                    for (Agent agent : agents) {
+                        agent.update(); 
+                    }
+                    changed();public void suspend() {
+        for (Agent agent : agents) {
+            agent.suspend();
+        }
+    }
+
+    public void resume() {
+        for (Agent agent : agents) {
+            agent.resume();
+        }
+    }
+
+    public void stop() {
+        for (Agent agent : agents) {
+            agent.stop();
+        }
+        agents.clear(); // Clear the list of agents
+    }
+      
 
 }
