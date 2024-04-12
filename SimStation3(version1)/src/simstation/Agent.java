a3e1604ea9eeaf80d785b4c14ed37414587a1e45
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-11 17:36:04 -0700

    Update Agent.java

--- SimStation3(version1)/src/simstation/Agent.java
+++ SimStation3(version1)/src/simstation/Agent.java
@@ -3,13 +3,13 @@ package simstation;
 import mvc.*;
 
 public abstract class Agent implements Runnable {
-    protected int x, y; 
-    protected Heading heading; 
-    protected Simulation world; 
+    protected int x, y;
+    protected Heading heading;
+    protected Simulation world;
     protected boolean suspended;
     protected Thread myThread;
 
-    protected boolean isRunning; 
+    protected boolean isRunning;
 
 
     public Agent() {
@@ -34,9 +34,9 @@ public abstract class Agent implements Runnable {
     public abstract void update();
 
     public void start() {
-        if (!isRunning) { 
+        if (!isRunning) {
             myThread.start();
-            isRunning = true; 
+            isRunning = true;
         }
     }
 
@@ -56,16 +56,16 @@ public abstract class Agent implements Runnable {
     protected void move(int steps) {
         switch (heading) {
             case NORTH:
-                y = Math.max(0, y - steps); 
+                y = Math.max(0, y - steps);
                 break;
             case SOUTH:
-                y = Math.min(Params.WORLD_HEIGHT, y + steps); 
+                y = Math.min(Params.WORLD_HEIGHT, y + steps);
                 break;
             case EAST:
-                x = Math.min(Params.WORLD_WIDTH, x + steps); 
+                x = Math.min(Params.WORLD_WIDTH, x + steps);
                 break;
             case WEST:
-                x = Math.max(0, x - steps); 
+                x = Math.max(0, x - steps);
                 break;
         }
     }
@@ -89,3 +89,100 @@ public abstract class Agent implements Runnable {
         heading = newHeading;
     }
 }
+
+
+
+
+
+
+// package simstation;
+
+// import mvc.*;
+
+// public abstract class Agent implements Runnable {
+//     protected int x, y; 
+//     protected Heading heading; 
+//     protected Simulation world; 
+//     protected boolean suspended;
+//     protected Thread myThread;
+
+//     protected boolean isRunning; 
+
+
+//     public Agent() {
+//         this.world = world;
+//         x = Utilities.rng.nextInt(Params.WORLD_WIDTH);
+//         y = Utilities.rng.nextInt(Params.WORLD_HEIGHT);
+//         suspended = false;
+//         myThread = new Thread(this);
+//     }
+
+//     public void run() {
+//         while (!suspended) {
+//             update();
+//             try {
+//                 Thread.sleep(Params.DELAY);
+//             } catch (InterruptedException e) {
+//                 onInterrupted();
+//             }
+//         }
+//     }
+
+//     public abstract void update();
+
+//     public void start() {
+//         if (!isRunning) { 
+//             myThread.start();
+//             isRunning = true; 
+//         }
+//     }
+
+//     public void suspend() {
+//         suspended = true;
+//     }
+
+//     public void resume() {
+//         suspended = false;
+//     }
+
+//     public void stop() {
+//         myThread.interrupt();
+//         onExit();
+//     }
+
+//     protected void move(int steps) {
+//         switch (heading) {
+//             case NORTH:
+//                 y = Math.max(0, y - steps); 
+//                 break;
+//             case SOUTH:
+//                 y = Math.min(Params.WORLD_HEIGHT, y + steps); 
+//                 break;
+//             case EAST:
+//                 x = Math.min(Params.WORLD_WIDTH, x + steps); 
+//                 break;
+//             case WEST:
+//                 x = Math.max(0, x - steps); 
+//                 break;
+//         }
+//     }
+
+
+//     protected void onInterrupted() {
+//     }
+
+//     protected void onExit() {
+//     }
+
+//     public int getX() {
+//         return x;
+//     }
+
+//     public int getY() {
+//         return y;
+//     }
+
+//     public void changeHeading(Heading newHeading) {
+//         heading = newHeading;
+//     }
+// }
