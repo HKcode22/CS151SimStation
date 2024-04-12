6b5f56c6dd95fa4164d3874edabecaa20c1262d7
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-11 23:49:54 -0700

    worked on plagueSimulation

--- SimStation3(version1)/src/plague/PlagueSimulation.java
+++ SimStation3(version1)/src/plague/PlagueSimulation.java
@@ -1,9 +1,21 @@
 package plague;
+import mvc.*;
 import simstation.*;
 
 // WORK IN PROGRESS
 public class PlagueSimulation extends Simulation {
     public static int VIRULENCE = 50; // % chance of infection
     public static int RESISTANCE = 2; // % chance of resisting infection
-    // etc.
+
+    public void populate() {
+        for(int i = 0; i < 2; i++) // add the infected
+            addAgent(new Host());
+        for(int i = 0; i < 28; i++) // add the healthy
+            addAgent(new Host());
+    }
+
+    public static void main(String[] args) {
+        AppPanel panel = new SimulationPanel(new PlagueFactory());
+        panel.display();
+    }
 }
