fc572a0554bd09259bc33598747d53b1e4ed8ac1
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-11 23:43:41 -0700

    started working on plague (WIP)

---
+++ SimStation3(version1)/src/plague/Host.java
@@ -0,0 +1,10 @@
+package plague;
+import simstation.*;
+
+// WORK IN PROGRESS
+public class Host extends Agent {
+    @Override
+    public void update() {
+
+    }
+}
---
+++ SimStation3(version1)/src/plague/PlagueSimulation.java
@@ -0,0 +1,9 @@
+package plague;
+import simstation.*;
+
+// WORK IN PROGRESS
+public class PlagueSimulation extends Simulation {
+    public static int VIRULENCE = 50; // % chance of infection
+    public static int RESISTANCE = 2; // % chance of resisting infection
+    // etc.
+}
