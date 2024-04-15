2c3c4750998c01c00f08696adba36253d0278437
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-14 22:27:24 -0700

    uploaded plagueView

---
+++ SimStation3(version1)/src/plague/PlagueView.java
@@ -0,0 +1,39 @@
+package plague;
+
+import mvc.*;
+import simstation.*;
+
+import java.awt.*;
+import java.util.List;
+
+public class PlagueView extends SimulationView {
+    private Simulation simulation;
+    public PlagueView(Model model) {
+        super(model);
+        if (model instanceof PlagueSimulation) {
+            this.simulation = (PlagueSimulation) model;
+        } else {
+            throw new IllegalArgumentException("Model must be an instance of PlagueSimulation");
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        List<Agent> agents = simulation.getAgents();
+        for (Agent agent : agents) {
+            if(agent instanceof Organism) {
+                Organism org = (Organism) agent;
+                if (org.isInfected())
+                    g.setColor(Color.RED);
+                else
+                    g.setColor(Color.GREEN);
+                g.fillOval(org.getX(), org.getY(), 10, 10); // Draw a small oval as a dot
+            }
+        }
+    }
+}
+
+
+
+
