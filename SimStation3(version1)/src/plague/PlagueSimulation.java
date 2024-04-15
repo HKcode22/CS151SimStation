249ac4a7030d017bbc89eec86c44b292fee9fb52
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-14 22:25:13 -0700

    fixed plageSimulation

--- SimStation3(version1)/src/plague/PlagueSimulation.java
+++ SimStation3(version1)/src/plague/PlagueSimulation.java
@@ -2,16 +2,15 @@ package plague;
 import mvc.*;
 import simstation.*;
 
-// WORK IN PROGRESS
 public class PlagueSimulation extends Simulation {
     public static int VIRULENCE = 50; // % chance of infection
     public static int RESISTANCE = 2; // % chance of resisting infection
 
     public void populate() {
-        for(int i = 0; i < 2; i++) // add the infected
-            addAgent(new Host());
-        for(int i = 0; i < 28; i++) // add the healthy
-            addAgent(new Healthy());
+        for (int i = 0; i < 100; i++) {
+            boolean initialInfect = Math.random() < 0.5; // 50% chance of initial infection
+            this.addAgent(new Organism(initialInfect));
+        }
     }
 
     public static void main(String[] args) {
