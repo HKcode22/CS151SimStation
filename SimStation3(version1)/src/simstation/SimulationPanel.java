68c51cb0e2ca1cceb78ccb9db836651390144017
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-15 04:03:08 -0700

    Update SimulationPanel.java

--- SimStation3(version1)/src/simstation/SimulationPanel.java
+++ SimStation3(version1)/src/simstation/SimulationPanel.java
@@ -13,16 +13,70 @@ public class SimulationPanel extends AppPanel {
         JButton suspendButton = new JButton("Suspend");
         JButton resumeButton = new JButton("Resume");
         JButton stopButton = new JButton("Stop");
+        JButton stats = new JButton("Stats");
 
         startButton.addActionListener(this);
+        suspendButton.addActionListener(this);
+        resumeButton.addActionListener(this);
+        stopButton.addActionListener(this);
+        stats.addActionListener(this);
         controlPanel.add(startButton);
         controlPanel.add(suspendButton);
         controlPanel.add(resumeButton);
         controlPanel.add(stopButton);
+        controlPanel.add(stats);
+
 
         view.setBackground(Color.GRAY);
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package simstation;
+// import mvc.*;
+
+// import javax.swing.*;
+// import java.awt.*;
+
+// public class SimulationPanel extends AppPanel {
+
+//     public SimulationPanel(AppFactory factory) {
+//         super(factory);
+
+//         JButton startButton = new JButton("Start");
+//         JButton suspendButton = new JButton("Suspend");
+//         JButton resumeButton = new JButton("Resume");
+//         JButton stopButton = new JButton("Stop");
+
+//         startButton.addActionListener(this);
+//         controlPanel.add(startButton);
+//         controlPanel.add(suspendButton);
+//         controlPanel.add(resumeButton);
+//         controlPanel.add(stopButton);
+
+//         view.setBackground(Color.GRAY);
+//     }
+
+// }
 
 
