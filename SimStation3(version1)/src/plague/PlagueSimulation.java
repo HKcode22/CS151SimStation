0cd1dcf96ee752efa007d7db327b98d14c5b4cc4
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-16 14:32:49 -0700

    added plague stats

--- SimStation3(version1)/src/plague/PlagueSimulation.java
+++ SimStation3(version1)/src/plague/PlagueSimulation.java
@@ -1,15 +1,41 @@
 package plague;
 import mvc.*;
 import simstation.*;
+import javax.swing.*;
 
 public class PlagueSimulation extends Simulation {
-    public static int VIRULENCE = 50; // % chance of infection
-    public static int RESISTANCE = 25; // % chance of resisting infection
+    public static int VIRULENCE = 25; // % chance of infection
+    public static int RESISTANCE = 25; // % chance of being resistant
 
     public void populate() {
         for (int i = 0; i < 30; i++) {
-            this.addAgent(new Organism(this));
+            addAgent(new Organism(this));
         }
+    }
+
+    @Override
+    public void stats() {
+        int totalAgents = getAgents().size();
+        int count = 0;
+
+        for (Agent agent : getAgents()) {
+            Organism organism = (Organism) agent;
+            if (organism.isInfected()) {
+                count++;
+            }
+        }
+
+        double percent = 0;
+        if (totalAgents > 0) {
+            percent = (double) count / totalAgents * 100; // Calculate age of infected agents
+        }
+
+        StringBuilder statsBuilder = new StringBuilder();
+        statsBuilder.append("# agents = ").append(totalAgents).append("\n");
+        statsBuilder.append("clock = ").append(this.clock).append("\n");
+        statsBuilder.append("% infected = ").append(String.format("%.2f%%", percent));
+
+        JOptionPane.showMessageDialog(null, statsBuilder.toString(), "Infection Stats", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void main(String[] args) {
