3fe4eecac0bd115444700cb8d0adcfcd53bb89a3
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-15 17:39:10 -0700

    Update Agent.java

--- SimStation3(version1)/src/simstation/Agent.java
+++ SimStation3(version1)/src/simstation/Agent.java
@@ -23,7 +23,8 @@ public abstract class Agent implements Runnable {
     public void run() {
         onStart();
         while (!suspended) {
-            update();
+//            update();
+//            change();
             try {
                 Thread.sleep(Params.DELAY);
             } catch (InterruptedException e) {
@@ -40,6 +41,12 @@ public abstract class Agent implements Runnable {
             myThread.start();
             isRunning = true;
         }
+    }
+
+    public double distancetonextagent(Agent otheragent){
+        int dx = this.x - otheragent.getX();
+        int dy = this.y - otheragent.getY();
+        return Math.sqrt(dx*dx+dy*dx);
     }
 
     public void suspend() {
@@ -94,4 +101,105 @@ public abstract class Agent implements Runnable {
         heading = newHeading;
     }
 }
+
+
+
+
+
+// package simstation;
+
+// import mvc.*;
+
+// public abstract class Agent implements Runnable {
+//     protected int x, y;
+//     protected Heading heading;
+//     protected Simulation world;
+//     protected boolean suspended;
+//     transient protected Thread myThread;
+
+//     protected boolean isRunning;
+
+
+//     public Agent() {
+//         this.world = world;
+//         x = Utilities.rng.nextInt(Params.WORLD_WIDTH);
+//         y = Utilities.rng.nextInt(Params.WORLD_HEIGHT);
+//         suspended = false;
+//         myThread = new Thread(this);
+//     }
+
+//     public void run() {
+//         onStart();
+//         while (!suspended) {
+//             update();
+//             try {
+//                 Thread.sleep(Params.DELAY);
+//             } catch (InterruptedException e) {
+//                 onInterrupted();
+//             }
+//         }
+//         onExit();
+//     }
+
+//     public abstract void update();
+
+//     public void start() {
+//         if (!isRunning) {
+//             myThread.start();
+//             isRunning = true;
+//         }
+//     }
+
+//     public void suspend() {
+//         suspended = true;
+//     }
+
+//     public void resume() {
+//         suspended = false;
+//     }
+
+//     public void stop() {
+//         myThread.interrupt();
+//         onExit();
+//     }
+
+//     protected void move(int steps) {
+//         switch (heading) {
+//             case NORTH:
+//                 y = Math.max(0, y - steps);
+//                 break;
+//             case SOUTH:
+//                 y = Math.min(Params.WORLD_HEIGHT, y + steps);
+//                 break;
+//             case EAST:
+//                 x = Math.min(Params.WORLD_WIDTH, x + steps);
+//                 break;
+//             case WEST:
+//                 x = Math.max(0, x - steps);
+//                 break;
+//         }
+//         world.changed();
+//     }
+
+//     protected void onStart(){
+//     }
+
+//     protected void onInterrupted() {
+//     }
+
+//     protected void onExit() {
+//     }
+
+//     public int getX() {
+//         return x;
+//     }
+
+//     public int getY() {
+//         return y;
+//     }
+
+//     public void changeHeading(Heading newHeading) {
+//         heading = newHeading;
+//     }
+// }
 
