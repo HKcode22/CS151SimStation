27ed3bfc148dc0fdbd5726362ba6d8f0f91f8d0d
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-15 18:26:06 -0700

    deleted unnecessary commented code

--- SimStation3(version1)/src/plague/Organism.java
+++ SimStation3(version1)/src/plague/Organism.java
@@ -1,4 +1,3 @@
-
 package plague;
 import mvc.*;
 import simstation.*;
@@ -8,10 +7,6 @@ public class Organism extends Agent {
     private boolean infected, resistant;
     public Simulation world;
 
-//    public Organism(Simulation world){
-//        this.world = world;
-//
-//    }
     public Organism(Simulation world){
         this.world = world;
         resistant = Utilities.rng.nextInt(100) < PlagueSimulation.RESISTANCE;
@@ -30,20 +25,6 @@ public class Organism extends Agent {
         infected = b;
     }
 
-//    @Override
-//    public void update() {
-//        if (infected) {
-//            // try to infect a neighbor
-//            Agent neighbor = world.getNeighbor(this, 10);
-//            if (neighbor != null && neighbor instanceof Organism) {
-//                Organism orgNeighbor = (Organism) neighbor;
-//                if (Utilities.rng.nextInt(100) < PlagueSimulation.VIRULENCE && !orgNeighbor.isResistant()) {
-//                    orgNeighbor.setInfected(true); // organism fails to resist
-//                }
-//            }
-//        }
-//    }
-
     public void update() {
         if (infected) {
             List<Agent> neighbors = world.getNeighbor(this, 2000);
@@ -58,47 +39,3 @@ public class Organism extends Agent {
     }
 
 }
-
-
-
-
-// package plague;
-// import mvc.*;
-// import simstation.*;
-
-// public class Organism extends Agent {
-//     private boolean infected, resistant;
-
-//     public Organism(){
-//         resistant = Utilities.rng.nextInt(100) < PlagueSimulation.RESISTANCE;
-//         infected = !resistant && Utilities.rng.nextInt(100) < PlagueSimulation.VIRULENCE;
-//     }
-
-//     public boolean isInfected() {
-//         return infected;
-//     }
-
-//     public boolean isResistant(){
-//         return resistant;
-//     }
-
-//     public void setInfected(boolean b){
-//         infected = b;
-//     }
-
-//     @Override
-//     public void update() {
-//         if (infected) {
-//             // try to infect a neighbor
-//             Agent neighbor = world.getNeighbor(this, 10);
-//             if (neighbor != null && neighbor instanceof Organism) {
-//                 Organism orgNeighbor = (Organism) neighbor;
-//                 if (Utilities.rng.nextInt(100) < PlagueSimulation.VIRULENCE && !orgNeighbor.isResistant()) {
-//                     orgNeighbor.setInfected(true); // organism fails to resist
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// }
