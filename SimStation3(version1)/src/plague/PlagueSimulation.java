e1bae57da1510387a0d69b3736867ae318d65b42
HKcode22 <139022998+HKcode22@users.noreply.github.com>
2024-04-15 17:46:28 -0700

    Update PlagueSimulation.java

--- SimStation3(version1)/src/plague/PlagueSimulation.java
+++ SimStation3(version1)/src/plague/PlagueSimulation.java
@@ -1,19 +1,51 @@
 package plague;
 import mvc.*;
+import randomWalks.*;
+import randomWalks.RandomWalkSimulation;
 import simstation.*;
 
 public class PlagueSimulation extends Simulation {
     public static int VIRULENCE = 50; // % chance of infection
     public static int RESISTANCE = 2; // % chance of resisting infection
 
+    public PlagueSimulation() {
+        populate();
+    }
+
     public void populate() {
         for (int i = 0; i < 25; i++) {
-            this.addAgent(new Organism());
+            this.addAgent(new Organism(this));
         }
     }
 
+
     public static void main(String[] args) {
+        SimulationView view = new SimulationView(new PlagueSimulation());
         AppPanel panel = new SimulationPanel(new PlagueFactory());
         panel.display();
     }
 }
+
+
+
+
+
+// package plague;
+// import mvc.*;
+// import simstation.*;
+
+// public class PlagueSimulation extends Simulation {
+//     public static int VIRULENCE = 50; // % chance of infection
+//     public static int RESISTANCE = 2; // % chance of resisting infection
+
+//     public void populate() {
+//         for (int i = 0; i < 25; i++) {
+//             this.addAgent(new Organism());
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         AppPanel panel = new SimulationPanel(new PlagueFactory());
+//         panel.display();
+//     }
+// }
