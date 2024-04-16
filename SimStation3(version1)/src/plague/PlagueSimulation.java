375ac04b34c32e6edbceccf7e2811dca82f388a3
Sandy Tam <111902534+satam2@users.noreply.github.com>
2024-04-16 13:47:02 -0700

    fix clashes

--- SimStation3(version1)/src/plague/PlagueSimulation.java
+++ SimStation3(version1)/src/plague/PlagueSimulation.java
@@ -1,51 +1,19 @@
 package plague;
 import mvc.*;
-import randomWalks.*;
-import randomWalks.RandomWalkSimulation;
 import simstation.*;
 
 public class PlagueSimulation extends Simulation {
     public static int VIRULENCE = 50; // % chance of infection
-    public static int RESISTANCE = 2; // % chance of resisting infection
-
-    public PlagueSimulation() {
-        populate();
-    }
+    public static int RESISTANCE = 25; // % chance of resisting infection
 
     public void populate() {
-        for (int i = 0; i < 25; i++) {
+        for (int i = 0; i < 30; i++) {
             this.addAgent(new Organism(this));
         }
     }
 
-
     public static void main(String[] args) {
-        SimulationView view = new SimulationView(new PlagueSimulation());
         AppPanel panel = new SimulationPanel(new PlagueFactory());
         panel.display();
     }
 }
-
-
-
-
-
-// package plague;
-// import mvc.*;
-// import simstation.*;
-
-// public class PlagueSimulation extends Simulation {
-//     public static int VIRULENCE = 50; // % chance of infection
-//     public static int RESISTANCE = 2; // % chance of resisting infection
-
-//     public void populate() {
-//         for (int i = 0; i < 25; i++) {
-//             this.addAgent(new Organism());
-//         }
-//     }
-
-//     public static void main(String[] args) {
-//         AppPanel panel = new SimulationPanel(new PlagueFactory());
-//         panel.display();
-//     }
-// }
